326fcd97eac995c11b84dca61ea856a8669a8c2b
Kade Allen <kadeallenprofessional@gmail.com>
2019-02-19 16:54:58 +1300

    Basic Layouts

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
 exports.getIndex = (req, res) => {
-  return res.render('index.ejs')
+  return res.render('index.ejs', { 
+    pageTitle: 'Draft History',
+    headerTitle: 'Draft History'
+  })
 }
