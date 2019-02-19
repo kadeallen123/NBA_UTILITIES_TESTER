f415db95a089609ca7bf2a66f2f38dceff7d618c
Kade Allen <kadeallenprofessional@gmail.com>
2019-02-19 17:11:33 +1300

    Changed Container Syling

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,6 +3,6 @@ const path = require('path')
 exports.getIndex = (req, res) => {
   return res.render('index.ejs', { 
     pageTitle: 'Draft History',
-    headerTitle: 'Draft History'
+    headerTitle: false
   })
 }
